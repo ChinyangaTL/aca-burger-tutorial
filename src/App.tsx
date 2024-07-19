38cf42c74ff0f67a9778067f3658b5dece9bd5b1
Les Chinyanga <les.chinyanga@gmail.com>
2024-07-19 11:58:38 +0200

    refactor: have inintial state as array at load

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import BaseBread from "./components/Basebread";
 import Filling from "./components/Filling";
 import TopBread from "./components/TopBread";
 
+const initialFilling = [
+  { color: "red", type: "Tomato" },
+  { color: "green", type: "Lettuce" },
+  { color: "brown", type: "Meat" },
+];
+
 const App = () => {
+  const [filling, setFilling] = useState(initialFilling);
+
   return (
     <div>
       <TopBread />
-      <Filling color="red" type="tomato" />
-      <Filling color="green" type="lettuce" />
-      <Filling color="brown" type="meat" />
+      {filling.map((layer) => {
+        return (
+          <>
+            <Filling type={layer.type} color={layer.color} />
+          </>
+        );
+      })}
 
       <BaseBread />
     </div>
